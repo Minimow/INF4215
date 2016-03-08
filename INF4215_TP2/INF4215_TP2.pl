749bd62918dcdcb1dc21c529f8ae706256026888
Julien <julien.bergeron9@gmail.com>
2016-03-08 17:41:32 -0500

    Premiere tentative

--- INF4215_TP2/INF4215_TP2.pl
+++ INF4215_TP2/INF4215_TP2.pl
@@ -1,8 +1,20 @@
-cours(LOG1000).
-cours(LOG3000).
-cours(LOG3900).
-cours(LOG3215).
-cours(INF1995).
-cours(INF4215).
+etudiant(julien).
 
+cours(log1000).
+cours(log2000).
+cours(log3000).
+
+requis(log3000, log2000).
+reussi(log1000, julien).
+
+elligible(cours, etudiant) :-
+	cours(cours),
+	etudiant(etudiant),
+	requis(cours, requis),
+	reussi(requis, etudiant).
+
+elligible(cours, etudiant) :-
+	cours(cours),
+	etudiant(etudiant),
+	true \= requis(cours, requis).
 
