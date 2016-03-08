3e0679acd79d8e60930d9e2d2539a607602e2ba8
Julien <julien.bergeron9@gmail.com>
2016-03-08 18:02:54 -0500

    Cours requis fonctionne s'il y a qu'un seul cours requis

--- INF4215_TP2/INF4215_TP2.pl
+++ INF4215_TP2/INF4215_TP2.pl
@@ -4,17 +4,18 @@ cours(log1000).
 cours(log2000).
 cours(log3000).
 
+requis(log2000, log1000).
 requis(log3000, log2000).
+
 reussi(log1000, julien).
 
-elligible(cours, etudiant) :-
-	cours(cours),
-	etudiant(etudiant),
-	requis(cours, requis),
-	reussi(requis, etudiant).
+elligible(Cours, Etudiant) :-
+	cours(Cours),
+	etudiant(Etudiant),
+	requis(Cours, Requis),
+	reussi(Requis, Etudiant).
 
-elligible(cours, etudiant) :-
-	cours(cours),
-	etudiant(etudiant),
-	true \= requis(cours, requis).
-
+elligible(Cours, Etudiant) :-
+	cours(Cours),
+	etudiant(Etudiant),
+	\+ requis(Cours, _).
